41b67a64af2ea8e842ecfa79754bbf8393d31596
NavBGitHub <sqa.navnath@gmail.com>
2023-05-29 11:02:23 +0530

    Added assertions

--- src/test/java/InMemoryStore.java
+++ src/test/java/InMemoryStore.java
@@ -1,4 +1,7 @@
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.Arrays;
 
 public class InMemoryStore {
 
@@ -12,14 +15,18 @@ public class InMemoryStore {
         store.addSong("Song 3","User A");
 
         System.out.println("Song Playlist Looks like: "+store.getRecentlyPlayedSongs("User A"));
+        Assert.assertEquals(store.getRecentlyPlayedSongs("User A"), Arrays.asList("Song 1", "Song 2", "Song 3"));
 
         store.addSong("Song 4","User A");
         System.out.println("When Song 4 is played: "+store.getRecentlyPlayedSongs("User A"));
+        Assert.assertEquals(store.getRecentlyPlayedSongs("User A"), Arrays.asList("Song 2", "Song 3", "Song 4"));
 
         store.addSong("Song 2","User A");
         System.out.println("When Song 2 is played: "+store.getRecentlyPlayedSongs("User A"));
+        Assert.assertEquals(store.getRecentlyPlayedSongs("User A"), Arrays.asList("Song 3", "Song 4", "Song 2"));
 
         store.addSong("Song 1","User A");
         System.out.println("When Song 1 is played: "+store.getRecentlyPlayedSongs("User A"));
+        Assert.assertEquals(store.getRecentlyPlayedSongs("User A"), Arrays.asList("Song 4", "Song 2", "Song 1"));
     }
 }
